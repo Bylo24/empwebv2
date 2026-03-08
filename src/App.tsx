2a808f93cdd35be5361ad502f19cd880218a8789
bylo24 <bylobtw@gmail.com>
2026-03-08 17:02:17 +1300

    Update all files

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import WorkWithUsPage from "./pages/WorkWithUs";
 import WorkWithUsPage from "./components/WorkWithUs";
 import VideoUpload from "./pages/VideoUpload";
-<<<<<<< HEAD
 import V1WebLanding from "./pages/V1WebLanding";
-=======
->>>>>>> origin/main
 import VideoViewer from "./pages/VideoViewer";
 import { VideoLibraryProvider } from "@/context/VideoLibraryContext";
 
@@ -30,10 +26,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/work-with-us" element={<WorkWithUsPage />} />
           <Route path="/upload" element={<VideoUpload />} />
-<<<<<<< HEAD
           <Route path="/v1weblanding" element={<V1WebLanding />} />
-=======
->>>>>>> origin/main
           <Route path="/video/:slug" element={<VideoViewer />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
